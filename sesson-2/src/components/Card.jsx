6def0c6aa4adb6d9dfb17402158c704a4b46558a
bsdaoquang <bsdaoquang@gmail.com>
2024-03-03 22:03:53 +0700

    add component

---
+++ sesson-2/src/components/Card.jsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import React from 'react';
+
+const Card = () => {
+	return (
+		<div className='block'>
+			<h1>B</h1>
+		</div>
+	);
+};
+
+export default Card;
---
+++ sesson-2/src/screens/HomeScreen.jsx
@@ -0,0 +1,24 @@
+/** @format */
+
+import React from 'react';
+import Card from '../components/Card';
+
+const HomeScreen = () => {
+	return (
+		<div>
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+		</div>
+	);
+};
+
+export default HomeScreen;
