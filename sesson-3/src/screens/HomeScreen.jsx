a01adc1863509baef94552e73d062a85bba4003e
bsdaoquang <bsdaoquang@gmail.com>
2024-03-17 21:33:08 +0700

    render list

--- sesson-3/src/screens/HomeScreen.jsx
+++ sesson-3/src/screens/HomeScreen.jsx
@@ -1,9 +1,79 @@
 /** @format */
 
-import React from 'react';
+import React, { useState } from 'react';
+import { films } from '../data/films';
+import { Avatar, Button, List } from 'antd';
+import { FaHome } from 'react-icons/fa';
 
 const HomeScreen = () => {
-	return <div>HomeScreen</div>;
+	// const [data, setData] = useState(
+	// 	films.filter((element) => element.Metascore !== 'N/A')
+	// );
+
+	// const handleSort = (type) => {
+	// 	const items = [...data];
+
+	// 	items.sort((a, b) =>
+	// 		type === 'ab'
+	// 			? parseFloat(a.Metascore) - parseFloat(b.Metascore)
+	// 			: parseFloat(b.Metascore) - parseFloat(a.Metascore)
+	// 	);
+
+	// 	setData(items);
+	// };
+
+	return (
+		<div>
+			{/* <span>
+				Score: <button onClick={() => handleSort('ab')}>Nhỏ đến lớn</button>{' '}
+				<button onClick={() => handleSort()}>Lớn đến nhỏ</button>
+				<button
+					onClick={() =>
+						setData(films.filter((element) => element.Metascore !== 'N/A'))
+					}>
+					Clear
+				</button>
+			</span>
+
+			<ul>
+				{data.map((item, index) => (
+					<li>
+						<h2>
+							{item.Title} {item.Metascore}
+						</h2>
+						<p>{item.Plot}</p>
+						{item.Type === 'movie' ? 'Phim' : 'Playlist'}
+					</li>
+				))}
+			</ul> */}
+
+			<h1>Movie</h1>
+
+			<ul>
+				{films.map(
+					(item) =>
+						item.Type === 'movie' && (
+							<li>
+								{item.Title} - {item.Type}
+							</li>
+						)
+				)}
+			</ul>
+
+			<h1>Series</h1>
+
+			<ul>
+				{films
+					.filter((element) => element.Type === 'series')
+					.map((item) => (
+						<li>
+							{item.Title}
+							{item.Type}
+						</li>
+					))}
+			</ul>
+		</div>
+	);
 };
 
 export default HomeScreen;
